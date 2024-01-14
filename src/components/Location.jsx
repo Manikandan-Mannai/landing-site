39be15b6b0c1ab20a8a019ac84eec74871e165f4
Manikandan-Mannai <iamanikandan.mannai@gmail.com>
2024-01-14 20:10:05 +0530

    location changed

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -32,7 +32,7 @@ const Location = () => {
             <Left>
                 <Map>
                     <Iframe
-                        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d10970.109486265126!2d80.05290969770056!3d13.565047585924153!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1splot%20no%3A%20206%20-%20lake%20view%20serenity%20pudi%2C%20tada%20-%20524401.%20tirupathi%20district%2C%20andhra%20pradesh!5e0!3m2!1sen!2sin!4v1703932568301!5m2!1sen!2sin"
+                        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3878.3197861624626!2d80.04354237508713!3d13.577263986796616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTPCsDM0JzM4LjIiTiA4MMKwMDInNDYuMCJF!5e0!3m2!1sen!2sin!4v1705242464060!5m2!1sen!2sin"
                         width="100%"
                         height="500"
                         style={{ border: 0 }}
